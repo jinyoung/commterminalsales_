d8a50af9bc8c8083a41abedc0f2602a1420ffe8c
Jinyoung Jang <jyjang@uengine.org>
2024-10-17 15:26:40 +0900

    commit

--- frontend/src/components/listers/SpecSpecComparationCards.vue
+++ frontend/src/components/listers/SpecSpecComparationCards.vue
@@ -1,5 +1,5 @@
 <template>
-     <v-container>
+    <v-container>
         <v-card>
             <v-card-title>
                 핸드폰 스펙 비교
@@ -24,6 +24,13 @@
                         :rules="[rules.required]"
                         required
                     ></v-text-field>
+                    <v-select
+                        v-model="selectedManufacturer"
+                        :items="manufacturers"
+                        label="제조사"
+                        :rules="[rules.required]"
+                        required
+                    ></v-select>
                     <v-btn @click="saveSpecComparation" color="primary">스펙비교저장</v-btn>
                 </v-form>
             </v-card-text>
@@ -37,34 +44,53 @@
                     v-model="searchParams.color"
                     label="색상"
                 ></v-text-field>
-                <v-text-field
+                <v-select
                     v-model="searchParams.manufacturer"
+                    :items="manufacturers"
                     label="제조사"
-                ></v-text-field>
+                ></v-select>
                 <v-btn @click="getSpecDetail" color="primary">조회</v-btn>
                 <v-list>
                     <v-list-item-group>
+                        <template v-if="specDetails.length === 0">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>조회된 스펙이 없습니다.</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
                         <v-list-item v-for="(spec, index) in specDetails" :key="index">
                             <v-list-item-content>
                                 <v-list-item-title>{{ spec.spec }}</v-list-item-title>
                                 <v-list-item-subtitle>{{ spec.phoneColor }} - {{ spec.phoneType }}</v-list-item-subtitle>
+                                <Photo v-if="spec.image" v-model="spec.image" :editMode="false" />
+                                <v-btn @click="addToComparison(spec)" color="success">비교 추가</v-btn>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list-item-group>
                 </v-list>
             </v-card-text>
         </v-card>
+        <v-card>
+            <v-card-title>
+                비교 목록
+            </v-card-title>
+            <v-card-text>
+                <v-data-table
+                    :headers="comparisonHeaders"
+                    :items="comparisonList"
+                    class="elevation-1"
+                ></v-data-table>
+            </v-card-text>
+        </v-card>
     </v-container>
 </template>
 
 <script>
-
     const axios = require('axios').default;
-    
+
     export default {
         name: 'SpecSpecComparationManager',
-        
-
         data() {
             return {
                 valid: false,
@@ -73,23 +99,39 @@
                     options: '',
                     customerId: ''
                 },
+                selectedManufacturer: null,
+                manufacturers: [],
                 searchParams: {
                     color: '',
                     manufacturer: ''
                 },
                 specDetails: [],
+                comparisonList: [], // List to hold selected specs for comparison
+                comparisonHeaders: [
+                    { text: 'Spec', value: 'spec' },
+                    { text: 'Phone Color', value: 'phoneColor' },
+                    { text: 'Phone Type', value: 'phoneType' },
+                    { text: 'Image', value: 'image' }
+                ],
                 rules: {
                     required: value => !!value || '필수 항목입니다.'
                 }
             };
         },
-        methods:{
+        async created() {
+            this.manufacturers = await this.fetchManufacturers();
+        },
+        methods: {
+            async fetchManufacturers() {
+                return ['Samsung', 'Apple', 'LG', 'Nokia']; // Example manufacturers
+            },
             async saveSpecComparation() {
                 try {
                     const response = await axios.post('/specComparations', {
                         productId: this.specComparation.productId,
                         options: this.specComparation.options.split(','),
-                        customerId: this.specComparation.customerId
+                        customerId: this.specComparation.customerId,
+                        manufacturer: this.selectedManufacturer
                     });
                     console.log('스펙 비교 저장 성공:', response.data);
                 } catch (error) {
@@ -104,16 +146,20 @@
                             manufacturer: this.searchParams.manufacturer
                         }
                     });
-                    this.specDetails = response.data;
+                    this.specDetails = response.data._embedded.specs;
                     console.log('스펙 상세 조회 성공:', this.specDetails);
                 } catch (error) {
                     console.error('스펙 상세 조회 실패:', error);
                 }
+            },
+            addToComparison(spec) {
+                // Add the selected spec to the comparison list
+                this.comparisonList.push(spec);
+                console.log('비교 목록에 추가됨:', spec);
             }
         }
     };
 </script>
-
 
 <style>
     .video-card {
@@ -123,4 +169,3 @@
         margin-bottom:50px;
     }
 </style>
-
